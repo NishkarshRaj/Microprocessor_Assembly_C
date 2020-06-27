ea983c893d6c54dd79b3597788648305a86fddad
NishkarshRaj <500060720@stu.upes.ac.in>
2020-06-27 16:04:43 +0530

    LED Blinking Program

--- 8051-Programming/1-LED-Blink/led.c
+++ 8051-Programming/1-LED-Blink/led.c
@@ -1,11 +1,12 @@
 #include<reg51.h>
-sbit LED= P1^0; //SFR (Special function register bit) // Pin 0 of Port 1
+sbit LED= P1^0; 
 
-void cct_init(void); // P1 as output port
+void init(void); // P1 as output port
+void delay(int a);
 
 int main(void)
 {
-cct_init();
+init();
 while(1)
 {
 LED=0;
@@ -15,7 +16,7 @@ delay(30000);
 }
 }
 
-void cct_init(void)
+void init(void)
 {
 P1 = 0x00; // P1= 0000 0000 // Port 1 as output -- 0 implies output pins 1 implies input pins!
 }
